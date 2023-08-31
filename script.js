b86b3f5cd9af26231250426d43cdffc30c7ac87e
ducksboy <141316936+ducksboy@users.noreply.github.com>
2023-09-01 10:32:19 +1200

    Update script.js
    Setting up the JS to handle the form inputs, hopefully I'll soon figure out how to get the JS to send the information to an email address.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function clickListener(event) {
-	event.preventDefault();
-}
+let orderForm = document.getElementById("form");
+orderForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-let submitButton = document.querySelector("#submit-button");
-submitButton.addEventListener('click', clickListener);
-console.log(submitButton);
+	let email = document.getElementByName("email");
+	let orderInstructions = document.getElementById("order-instructions");
+
+	if(email.value == "" || orderInstructions.value == "") {
+		alert("Ensure you input a value in both fields");
+		//throw error
+	} else {
+		// perform operation with form input
+		alert("Your order has been successfully submitted!");
+		console.log(
+			email: ${email.value} order instructions: ${orderInstructions.value}
+		);
+		email.value="";
+		orderInstructions.value="";
+	}
+});
